afcf299f912961303a1704b0bfd806e6bec58f0a
pedrokpp <pedrokp@id.uff.br>
2021-10-12 17:34:42 -0300

    refactor & check de valores nulos

--- routes/users.js
+++ routes/users.js
@@ -21,20 +21,25 @@ router.post('/register', registerMiddleware, async (req, res) => {
 
     try {
         const newUser = await user.save()
-        res.status(201).send('usuário criado')
+        return res.status(201).send('usuário criado')
     } catch {
-        res.status(500).send('erro interno')
+        return res.status(500).send('erro interno')
     }
 
 })
 
+router.patch('/:username', checkAdminKey, (req, res) => {
+    
+})
+
 async function loginMiddleware(req, res, next) {
     let user = {}
+    
+    if (!req.body.username || !req.body.password) return res.status(400).send('body incompleto')
+
     try {
         user = await User.findOne({ "username": req.body.username })
-        if (!user) {
-            return res.status(400).send('usuário não existe')
-        }
+        if (!user) return res.status(400).send('usuário não existe')
     } catch {
         return res.status(500).send('erro interno')
     }
@@ -44,20 +49,24 @@ async function loginMiddleware(req, res, next) {
 
 async function registerMiddleware(req, res, next) {
     let user = {}
-    if (req.body.username.match("^[a-zA-Z0-9_]*$") == null) {
-        return res.status(400).send('username contém caracteres inválidos')
-    }
-    if (req.body.username.length > 16) {
-        return res.status(400).send('username muito grande')
-    }
+
+    if (!req.body.username || !req.body.password) return res.status(400).send('body incompleto')
+
+    if (req.body.username.length > 16) return res.status(400).send('username muito grande')
+    if (req.body.username.lengt < 3) return res.status(400).send('username muito pequeno')
+    if (req.body.username.match("^[a-zA-Z0-9_]*$") == null) return res.status(400).send('username contém caracteres inválidos')
+
     try {
         user = await User.findOne({ "username": req.body.username })
-        if (user) {
-            return res.status(400).send('usuário já existente')
-        }
+        if (user) return res.status(400).send('usuário já existente')
     } catch {
         return res.status(500).send('erro interno')
     }
+    next()
+}
+
+function checkAdminKey(req, res, next) {
+    if (req.body.auth !== process.env.ADMIN_KEY) return res.status(401).send("acesso não autorizado")
     next()
 }
 
