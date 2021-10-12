ead1197a780f528660e84a2b6b20c42e2ecba1a6
pedrokpp <pedrokp@id.uff.br>
2021-10-12 13:31:12 -0300

    projeto iniciado

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+
+db.on('error', error => console.error("dbs error: " + error))
+db.once('open', () => console.log("dbs conectada"))
+
+app.use(express.json())
+
+app.listen(3000, () => console.log("api listening at port 3000"))
