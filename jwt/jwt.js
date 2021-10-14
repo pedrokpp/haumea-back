421f8669b6f63881063dbc0c4d355b83f68f17de
pedrokpp <pedrokp@id.uff.br>
2021-10-13 21:20:58 -0300

    lidar com tokens expirados

--- jwt/jwt.js
+++ jwt/jwt.js
@@ -1,8 +1,22 @@
 const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const fs = require('fs')
 
 function generateAccessToken(user) {
     return jwt.sign(user, process.env.JWT_TOKEN, { expiresIn: '7d' })
 }
 
-module.exports = { generateAccessToken }
+function isTokenValid(token) {
+    let expiredTokens = fs.readFileSync('./expired-tokens.txt', { encoding: 'utf-8' }).trim().split("\n")
+    if (expiredTokens.includes(token)) return false
+    
+    let value = jwt.verify(token, process.env.JWT_TOKEN)
+
+    if (value == null) {
+        fs.appendFileSync('./expired-tokens.txt', `${token}\n`)
+        return false
+    }
+    
+    return true
+}
+
+module.exports = { generateAccessToken, isTokenValid }
