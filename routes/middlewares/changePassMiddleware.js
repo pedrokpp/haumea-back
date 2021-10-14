4422f5bf2cdd8e85a1154c3d1903459a22b568e5
pedrokpp <pedrokp@id.uff.br>
2021-10-14 19:13:36 -0300

    changePassMiddleware concluído

--- routes/middlewares/changePassMiddleware.js
+++ routes/middlewares/changePassMiddleware.js
@@ -1,4 +1,6 @@
 const User = require("../../models/user");
+const expiredTokens = require("../models/expiredTokens"); 
+const bcrypt = require("bcrypt");
 
 async function changePassMiddleware(req, res, next) {
     if (!req.body.username || !req.body.password || !req.body.newPassword)
@@ -6,8 +8,20 @@ async function changePassMiddleware(req, res, next) {
 
     const user = await User.findOne({ username: req.body.username });
 
+    if (!user) return res.status(403).send("acesso bloqueado");
+
     if (!(await bcrypt.compare(req.body.password, user.password)))
         return res.status(401).send("senha inválida");
+
+    const sessionToken = req.cookies.session;
+    
+    if (sessionToken != undefined && sessionToken) {
+        res.clearCookie("session");
+        const newExpiredToken = new expiredTokens({
+            token: sessionToken,
+        });
+        await newExpiredToken.save(); // lure tem que dar redirect pra main page
+    }
 
     next();
 }
