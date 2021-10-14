e00734a2bf18b91b264c6e3936aa896a9fb4e786
pedrokpp <pedrokp@id.uff.br>
2021-10-14 19:07:08 -0300

    format code

--- routes/middlewares/loginMiddleware.js
+++ routes/middlewares/loginMiddleware.js
@@ -7,11 +7,11 @@ async function loginMiddleware(req, res, next) {
     if (!req.body.username || !req.body.password)
         return res.status(400).send("body incompleto");
 
-    const sessionToken = req.cookies.session
+    const sessionToken = req.cookies.session;
 
     if (sessionToken != undefined && sessionToken) {
         if (!(await jwt.isTokenValid(sessionToken))) {
-            res.clearCookie('session');
+            res.clearCookie("session");
             return res.status(403).send("token inválido, crie uma nova sessão");
         }
         return res.status(200).send("logado com sucesso via token");
