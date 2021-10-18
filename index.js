f21e737f54433e5c9b948b42ee82766b6da44365
pedrokpp <pedrokp@id.uff.br>
2021-10-18 16:42:24 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 const app = express();
 require("dotenv").config();
 
@@ -14,6 +15,7 @@ db.on("error", (error) => console.error("dbs error: " + error));
 db.once("open", () => console.log("dbs conectada"));
 
 app.use(express.json());
+app.use(cors())
 app.use(cookieParser());
 
 app.use("/users", require("./routes/users"));
