fc29f7b215d37e20c8cba23e4e4ded7415319e58
pedrokpp <pedrokp@id.uff.br>
2021-10-19 15:44:07 -0300

    melhor organização middlewares

--- routes/users.js
+++ routes/users.js
@@ -11,26 +11,17 @@ router.post("/register", require("./middlewares/registerMiddleWare"));
 
 router.patch("/change-password", require("./middlewares/changePassMiddleware"));
 
-router.post("/logout", async (req, res) => {
-    if (!req.body.token) return res.status(400).send("body incompleto");
-    const newExpiredToken = new Token({
-        token: req.body.token,
-    });
-    await newExpiredToken.save();
-    return res.status(200).send("token invalidado");
-});
+router.post("/logout", require("./middlewares/logoutMiddleware"));
 
-router.post("/validate-token", async (req, res) => {
-    if (!req.body.token) return res.status(400).send("body incompleto");
-
-    if (await jwt.isTokenValid(req.body.token))
-        return res.status(200).send("token válido");
-    return res.status(404).send("token inválido");
-});
+router.post(
+    "/validate-token",
+    require("./middlewares/validateTokenMiddleware")
+);
 
 router.delete(
     "/delete-account",
     require("./middlewares/deleteUserMiddleware"),
+    // TODO delete-account endpoint
     async (req, res) => {
         if (!(await bcrypt.compare(req.body.password, req.body.user.password)))
             return res.status(401).send("senha inválida");
@@ -40,20 +31,7 @@ router.delete(
 router.patch(
     "/:username",
     require("./middlewares/checkAdminKey"),
-    async (req, res) => {
-        let user = {};
-
-        try {
-            await User.updateOne(
-                { username: req.params.username },
-                { userLevel: req.body.userLevel }
-            );
-            user = await User.findOne({ username: req.params.username });
-            return res.status(200).json(user);
-        } catch {
-            return res.status(500).send("erro interno");
-        }
-    }
+    require("./middlewares/userLevelMiddleware")
 );
 
 module.exports = router;
