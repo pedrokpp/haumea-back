9d0085d2b0ef0d183a6f67839caf2e7bbd79d824
pedrokpp <pedrokp@id.uff.br>
2021-10-21 18:24:06 -0300

    fix unused vars

--- routes/users/middlewares/checkAdminKey.js
+++ routes/users/middlewares/checkAdminKey.js
@@ -1,9 +1,3 @@
-function checkAdminKey(req, res, next) {
-    if (req.body.auth && req.body.auth !== process.env.ADMIN_KEY)
-        return res.status(401).send("acesso não autorizado");
-    next();
-}
-
 module.exports = (req, res, next) => {
     if (req.body.auth && req.body.auth !== process.env.ADMIN_KEY)
         return res.status(401).send("acesso não autorizado");
