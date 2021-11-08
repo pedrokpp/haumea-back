8b6b8cbd2254f7f717f0972500c4446692847e10
pedrokpp <pedrokp@id.uff.br>
2021-11-08 19:42:54 -0300

    refactor contra null exception

--- routes/users/middlewares/loginMiddleware.js
+++ routes/users/middlewares/loginMiddleware.js
@@ -3,8 +3,6 @@ const jwt = require("../../../jwt/jwt");
 const bcrypt = require("bcrypt");
 
 module.exports = async (req, res, next) => {
-    let user = {};
-
     if (!req.body.username || !req.body.password)
         return res.status(400).send("body incompleto");
 
@@ -16,19 +14,19 @@ module.exports = async (req, res, next) => {
         return res.status(400).send("nome de usuário inválido"); // manter check para segurança contra exploits
 
     try {
-        user = await User.findOne({ username: req.body.username });
+        const user = await User.findOne({ username: req.body.username });
         if (!user) return res.status(400).send("usuário não existe");
+        
+        if (!(await bcrypt.compare(req.body.password, user.password)))
+            return res.status(401).send("senha inválida");
+
+        const token = jwt.generateAccessToken({
+            username: user.username,
+            userLevel: user.userLevel,
+        });
+
+        return res.status(200).json({ token: token });
     } catch {
         return res.status(500).send("erro interno");
     }
-
-    if (!(await bcrypt.compare(req.body.password, user.password)))
-        return res.status(401).send("senha inválida");
-
-    const token = jwt.generateAccessToken({
-        username: user.username,
-        userLevel: user.userLevel,
-    });
-
-    return res.status(200).json({ token: token });
 };
